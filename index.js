0c03264d13f76ea3adbb2c08a1dfe212dc170b1e
PRASANG <cheap@gmail.com>
2024-02-08 03:13:39 +0530

    Update Interface and Add multiple new features
    New Game Button is added at winning dialog
    Choice for turn of the player is added
    Indicator for player turn is added
    Double Click and any key pressed result in marker
    as per the indicator

--- index.js
+++ index.js
@@ -1,45 +1,241 @@
+let container = document.querySelector(".container");
+let cellList = document.querySelectorAll(".cellInput");
+let turnCount =0;
+
+
+// ['X','X','O','O','X','X','X','O','O']
+
 let GameBoard = (function (){
     let gameboard = new Array(9);
-    gameboard = ['X','X','O','O','O','O','X','O','O'];
+    gameboard = ['P','P','P','P','P','P','P','P','P'];
 
-    let cellList = document.querySelectorAll(".cellInput");
-
-    for(let i=0 ; i<9 ;i++){
-        cellList[i].addEventListener("change",()=>{
-            // let index = Number(cellList[i].getAttribute("data-index"));
-            gameboard[i] = cellList[i].value;
-            cellList[i].setAttribute("readOnly","true");
-        });
+    function reset(){
+        for(let i = 0 ; i<9 ; i++){
+            gameboard[i]='P';
+        }
     }
 
-    return {gameboard};
+    
+
+    for(let i=0 ; i<9 ;i++){
+        cellList[i].addEventListener("input",()=>{
+            
+            if(Game.playerOne.getLock()==1 && Game.playerTwo.getLock()==0){
+                Game.playerOne.lockSwitch();
+                Game.playerTwo.lockSwitch();
+                displayController.toggleColor(2);
+                fillAndCheck(i,Game.playerOne);
+                
+                
+            }
+            else if(Game.playerOne.getLock()==0 && Game.playerTwo.getLock()==1){
+                Game.playerTwo.lockSwitch();                
+                Game.playerOne.lockSwitch();
+                displayController.toggleColor(2);
+                fillAndCheck(i,Game.playerTwo);
+                
+            }
+     
+        });
+
+        cellList[i].addEventListener("dblclick",()=>{
+            cellList[i].dispatchEvent(new Event('input'));
+        });
+        
+        
+    }
+
+    function fillAndCheck(i,player){
+        gameboard[i]=player.marker;
+        cellList[i].value=player.marker;
+        cellList[i].setAttribute("readOnly","true");
+        
+        
+        turnCount++;
+
+        if(turnCount>=5){
+            let result = Game.checkResult(i,player);
+            displayController.satisfaction(result);
+        }
+    }
+    
+
+
+    return {gameboard,reset};
     })();
 
 
-let container = document.querySelector(".container");
+
+function Player(name,marker){
+    let lock=0;
+
+    function getLock(){
+        return lock;
+    }
+
+    function lockSwitch(){
+        if(lock==0){
+            lock=1;
+            return;
+        }
+        lock=0;
+        return;
+    }
+    function resetLock(){
+        lock=0;
+    }
+    return {name,marker,getLock,lockSwitch,resetLock};
+}
+
+
+
 
 let displayController = ( function(){
-    //  for(let i =0 ; i<9; i++){
-    //     let cell = document.createElement("div");
-    //     cell.setAttribute("class","cell");
+    //Modal dialog
+    let modal = document.querySelector("#detailsForm");
+    let startButton = document.querySelector(".startButton");
+
+    startButton.addEventListener("click",()=>{
+        modal.showModal();
+    });
+
+    //input fields
+    let playerOneName = document.querySelector("#playerOneName");
+    let playerTwoName = document.querySelector("#playerTwoName");
+
+    //labels for Name
+    let playerOne = document.querySelector(".playerOne");
+    let playerTwo = document.querySelector(".playerTwo");
+
+    let playButton = document.querySelector(".playButton button");
+    let replayButton = document.querySelector(".replayButton");
+
+    playButton.addEventListener("click",()=>{
+
+        resetDisplay();
+        GameBoard.reset();
+        Game.gameReset();
+        resetColor();
         
-    //     let cellInput = document.createElement("input")
-    //     cellInput.setAttribute("type","text");
-    //     cellInput.setAttribute("class","cellInput");
-    //     cellInput.setAttribute("value","");
-    //     cellInput.setAttribute("maxlength","1");
 
-    //     cell.appendChild(cellInput);
-    //     container.appendChild(cell);
+        playerOne.textContent = playerOneName.value;
+        playerTwo.textContent = playerTwoName.value;
+        Game.setName(playerOneName.value,playerTwoName.value);
 
-    //  }
+        
+
+        let checkedButton = document.querySelector("input[type='radio'][name=Choice]:checked");
+
+        Game.activate(checkTurn(checkedButton.value));
+        
+    });
+
+    replayButton.addEventListener("click",()=>{
+        winnerDialog.close();
+        startButton.dispatchEvent(new Event("click"));
+    });
+
+
+    //radio Buttons
+
     
+
+    function checkTurn(value){
+        
+        if(value=='x'){
+            return 'x';
+        }
+        else if(value=='o'){
+            return'o';
+        }
+    }
+
+    //class Toggle on the class
+    function toggleColor(flag){
+        if(flag==0){
+            playerOne.classList.toggle("playing");
+        }
+        else if(flag==1){
+            playerTwo.classList.toggle("playing");
+        }
+        else{
+            playerOne.classList.toggle("playing");
+            playerTwo.classList.toggle("playing");
+        }
+    } 
+
+    function resetColor(){
+        playerOne.classList.remove("playing");
+        playerTwo.classList.remove("playing");
+    }
+    
+    //Winner Modal Dialog
+    let winnerDialog = document.querySelector("#winnerAnnouncement");
+    let winner = document.querySelector("#winnerAnnouncement p");
+
+    function satisfaction(result){
+        if(result==true){
+            winner.textContent=`${Game.winner.name}  wins`;  
+             winnerDialog.showModal();
+        }
+        else if (result==false && Game.winner==100){
+            winner.textContent=`Tie`;
+            winnerDialog.showModal();
+        }
+        
+    }
+
+    //reset display
+    function resetDisplay(){
+        for(let i = 0; i<9;i++){
+            cellList[i].value="";
+        }
+    }
+    
+    
+
+    return {satisfaction,toggleColor,resetDisplay};
 })();
 
-function game(){
+
+
+let  Game = (function(){
     let gameboard = GameBoard.gameboard;
     let result = false;
+    let winner = 101;
 
+    let playerOne = Player("Player-1","x");
+    let playerTwo = Player("Player-2","o");
+
+
+    //activate
+    function activate(val){
+        if(val=='x'){
+            playerOne.lockSwitch();
+            displayController.toggleColor(0);
+        }
+        else{
+            playerTwo.lockSwitch();
+            displayController.toggleColor(1);
+        }
+    }
+    //setting the Name of the players
+
+    function setName(firstName,secondName){
+        playerOne.name = firstName;
+        playerTwo.name = secondName;
+    }
+
+
+    function gameReset(){
+        result=false;
+        this.winner=101;
+        playerOne.resetLock();
+        playerTwo.resetLock();
+        turnCount=0;
+    }
+
+    //function checking the provided conditions
     function checkingCondition(val,index,increment){
         let result = true;
         for(let i=0 ; i<3 ; i++){
@@ -51,6 +247,10 @@ function game(){
         }
         return result;
     }
+
+    
+
+
     // checks for the combination in checkingHorizontal row
     function checkingHorizontal(val,index){
         let start = 0;
@@ -63,14 +263,6 @@ function game(){
              start = 6;
         }
         else {}
-
-        // while (result == true && start <= end){
-        //     if(gameboard[start]!=val){
-        //         result =  false ; 
-                
-        //     }
-        //     start++;
-        // }
         
         result = checkingCondition(val,start,1);
         return result;
@@ -101,22 +293,10 @@ function game(){
         let result = true ;
         let start = 0;
         if(index==0 || index==4 || index==8){
-            // while ( result == true && start <= 8){
-            //     if(gameboard[start]!= val){
-            //         result=false;
-            //     }
-            //     start+=4;
-            // }
             result = checkingCondition(val,start,4);
         }
         else if (index==2 || index==4 || index==6){
             start = 2; 
-            // while ( result == true && start <= 6){
-            //     if(gameboard[start]!= val){
-            //         result=false;
-            //     }
-            //     start+=2;
-            // }
             result = checkingCondition(val,start,2);
         }
         else{}
@@ -125,20 +305,23 @@ function game(){
     }
 
 
-    let i =0;
-    for(i =0 ; i<9;i++){
-        if(i==0 || i==6 ||i==4 ||i==6 ||i==8){
-            result = checkingVertical(gameboard[i],i) || checkingHorizontal(gameboard[i],i) || checkingDiagonal(gameboard[i],i);
+    
+    function checkResult(i,player){
+        if(( i%2==0 )&&(checkingVertical(player.marker,i) || checkingHorizontal(player.marker,i) || checkingDiagonal(player.marker,i))){
+            this.winner = player ; 
+            return true;
         }
-        else{
-            result = checkingVertical(gameboard[i],i) || checkingHorizontal(gameboard[i],i);
+        else if (( i%2!=0 )&&(result = checkingVertical(player.marker,i) || checkingHorizontal(player.marker,i))){
+            this.winner = player ;
+            return true;
         }
-        
-        if ( result == true){
-            break;
+        else if(turnCount == 9){
+            this.winner = 100;
+            return false;
         }
+        return false;
     }
-    console.log(`${gameboard[i]} wins`);
-}
+    return {playerOne,playerTwo,setName,activate,gameReset,winner , checkResult};
+    
+})();
 
-game();
